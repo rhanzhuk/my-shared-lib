593cdb3530c8d0eb71e150d91e6568f92318867f
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-07 17:53:12 +0200

    test get token from Class

---
+++ src/VaultHelper.groovy
@@ -0,0 +1,11 @@
+class VaultHelper {
+    String VAULT_ADDR = 'http://65.108.210.185:8211'
+    String VAULT_USER = "docker-helper"
+    String VAULT_PASS = "docker-root"
+
+    String getToken() {
+        def vaultToken = sh (script: "curl -X POST --data '{\"password\" : \"'$VAULT_PASS'\"}' $VAULT_ADDR/v1/auth/userpass/login/$VAULT_USER | jq -r .auth.client_token")
+        println vaultToken
+        return vaultToken
+    }
+}
