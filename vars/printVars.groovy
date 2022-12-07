c504ff0eef6eb7f502ebf7fd9773132b78227b6a
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-07 17:43:22 +0200

    test get token from Class

--- vars/printVars.groovy
+++ vars/printVars.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(){
-    def token = new VaultHelper().getToken()
-    println "$token"
+    def token = new VaultHelper()
+    def tokenRes = token.getToken().toString()
+    println tokenRes
 
     // its work
     //println "$env.GIT_PREVIOUS_COMMIT"
