e29f655ab675cba6828b74dcd434189b1e7da32a
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-07 17:55:32 +0200

    test get token from Class

--- vars/printVars.groovy
+++ vars/printVars.groovy
@@ -1,10 +1,13 @@
 #!/usr/bin/env groovy
+//import VaultHelper
 
 def call(){
-    VaultHelper token = new VaultHelper()
-    def tokenRes = token.getToken()
-    println tokenRes
-    println "Test!"
+    String VAULT_ADDR = 'http://65.108.210.185:8211'
+    String VAULT_USER = "docker-helper"
+    String VAULT_PASS = "docker-root"
+    def token = sh (script: "curl -X POST --data '{\"password\" : \"'$VAULT_PASS'\"}' $VAULT_ADDR/v1/auth/userpass/login/$VAULT_USER | jq -r .auth.client_token")
+    println token
+    return token
 
     // its work
     //println "$env.GIT_PREVIOUS_COMMIT"
