8c56781c2d43ec61c61b9d8d1b700a1e825c138f
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-08 17:37:12 +0200

    final commit

--- vars/deployTok8s.groovy
+++ vars/deployTok8s.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-call() {
+def call() {
     def imageDep = GlobalVars.image
     sh (script: "ssh root@65.108.155.54 \"kubectl set image -n default deployment/spring-flyway-outside spring-flyway-outside=$imageDep\"")
 }
