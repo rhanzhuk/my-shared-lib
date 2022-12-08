d8d3d4e2598212dc3278ce4ba91d8206534e02b1
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-08 09:55:55 +0200

    rm not working method

--- vars/testVarFromAnotherScript.groovy
+++ vars/testVarFromAnotherScript.groovy
@@ -1,9 +1,8 @@
 #!/usr/bin/env groovy
 
 def call() {
-    def simpleVar = new printVars().run()
-    println simpleVar.toString()
-    println simpleVar
+    //def simpleVar = printVars
+    //println simpleVar
     //its work!!!
     def simpleVar2 = printVars.test()
     println simpleVar2
