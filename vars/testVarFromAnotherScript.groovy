d387dd5ff9ddfb254c9aa77a50d1ff0555b14d65
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-08 09:53:12 +0200

    test from another script

--- vars/testVarFromAnotherScript.groovy
+++ vars/testVarFromAnotherScript.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
 def call() {
-    def simpleVar = new printVars()
+    def simpleVar = new printVars().run()
     println simpleVar.toString()
+    println simpleVar
     //its work!!!
     def simpleVar2 = printVars.test()
     println simpleVar2
