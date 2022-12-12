b6ac9d30efcf4a4c9468850c664bb9e5b2e87415
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-12 14:28:45 +0200

    test work with jobs

--- vars/printJobsJenkins.groovy
+++ vars/printJobsJenkins.groovy
@@ -1,4 +1,7 @@
-jobs = jenkins.model.Jenkins.instance.getJobNames()
 
-println "Number of jobs: " + jobs.size()
-println "\nList of jobs:\n" + jobs.join("\n")
+def call(){
+    jobs = jenkins.model.Jenkins.instance.getJobNames()
+
+    println "Number of jobs: " + jobs.size()
+    println "\nList of jobs:\n" + jobs.join("\n")
+}
