f993edc25f093b1a32a57ebd06b17b2d714cbfdb
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-15 09:28:32 +0200

    test 2 method

--- vars/gitCheck.groovy
+++ vars/gitCheck.groovy
@@ -3,4 +3,10 @@
 def call (){
     HashMap scmVars = checkout scm
     scmVars.each{ k, v -> println "${k}:${v}" }
+    String a = testCall()
+    println a
+}
+
+def testCall() {
+    return "Test from another method from script!"
 }
