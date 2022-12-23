739f197e5462b569d179b4c10721ee79f613d0b3
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-12-23 17:12:16 +0200

    add node

--- vars/vault.groovy
+++ vars/vault.groovy
@@ -1,16 +1,17 @@
 #!/usr/bin/env groovy
 
 def call() {
-    withCredentials([usernamePassword(credentialsId: 'jenkins-auth-creds', usernameVariable: "USERNAME", passwordVariable: 'PASSWORD')]) {
-        println "Get token from library"
+    node ('master') {
+        withCredentials([usernamePassword(credentialsId: 'jenkins-auth-creds', usernameVariable: "USERNAME", passwordVariable: 'PASSWORD')]) {
+            println "Get token from library"
 
-        //println "username: ${USERNAME}"
-        //println "password: ${PASSWORD}"
+            //println "username: ${USERNAME}"
+            //println "password: ${PASSWORD}"
 
-        def token = sh(script: "curl --silent -X POST --data '{\"password\" : \"'${PASSWORD}'\"}' http://65.108.210.185:8211/v1/auth/userpass/login/${USERNAME} | jq -r .auth.client_token", returnStdout: true)
-        println token
+            def token = sh(script: "curl  -X POST --data '{\"password\" : \"'${PASSWORD}'\"}' http://65.108.210.185:8211/v1/auth/userpass/login/${USERNAME} | jq -r .auth.client_token", returnStdout: true)
+            println token
+        }
     }
-
 }
 
 def auth(){
