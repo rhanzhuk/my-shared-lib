7170e117f19102af89bbd558c27974380b615460
rhanzhuk <38084041+rhanzhuk@users.noreply.github.com>
2023-09-17 15:03:40 +0200

    Update VaultHelper.groovy

--- src/VaultHelper.groovy
+++ src/VaultHelper.groovy
@@ -1,7 +1,7 @@
 class VaultHelper {
     String VAULT_ADDR = 'http://65.108.210.185:8211'
-    String VAULT_USER = "docker-helper"
-    String VAULT_PASS = "docker-root"
+    String VAULT_USER = "****"
+    String VAULT_PASS = "****"
 
     String getToken() {
         def vaultToken = sh (script: "curl -X POST --data '{\"password\" : \"'$VAULT_PASS'\"}' $VAULT_ADDR/v1/auth/userpass/login/$VAULT_USER | jq -r .auth.client_token")
